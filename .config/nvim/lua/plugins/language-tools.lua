74bac94ea22feff5db74b8babd12af79fc78f1ce
Brobb954 <119805322+Brobb954@users.noreply.github.com>
2024-06-15 20:09:00 -0700

    ya know? more configuring of nvim

---
+++ .config/nvim/lua/plugins/language-tools.lua
@@ -0,0 +1,14 @@
+return {
+  { "mfussenegger/nvim-dap" },
+
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4",
+    config = function()
+      local bufnr = vim.api.nvim_get_current_buf()
+      vim.keymap.set("n", "<leader>a", function()
+        vim.cmd.RustLsp("codeAction")
+      end, { silent = true, buffer = bufnr })
+    end,
+  },
+}
