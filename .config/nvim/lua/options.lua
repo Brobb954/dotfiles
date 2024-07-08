a7497fc79929531368668d808eb64022768dc6b3
Brobb954 <119805322+Brobb954@users.noreply.github.com>
2024-07-08 16:03:23 -0700

    new nvim config testing

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,15 +1,6 @@
-vim.cmd(" set expandtab")
-vim.cmd(" set tabstop=2")
-vim.cmd(" set softtabstop=2")
-vim.cmd(" set shiftwidth=2")
-vim.opt.number = true
-vim.opt.relativenumber = true
+require "nvchad.options"
 
-vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+-- add yours here!
 
--- set leader key
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
